f366cb5875c6396104baf08eced2f3749753e079
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-09-23 12:52:36 +0800

    サーバ起動時にメッセージを出すようにした

--- server.js
+++ server.js
@@ -6,7 +6,9 @@
 
 const express = require('express');
 const app = express();
-const server = app.listen(8000);
+const server = app.listen(8000, ()=>{
+    console.log('Server listening on http://127.0.0.1:8000')
+});
 
 app.get('/*', (req, res, next)=>{
     res.send('<h1>Hello, world.</h1>');
