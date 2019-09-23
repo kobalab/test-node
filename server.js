58fe3b134b67fd293bf223e51000fe67eb05da10
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-09-23 12:51:27 +0800

    任意のURLで Hello, world を返すよう修正

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const express = require('express');
 const app = express();
 const server = app.listen(8000);
 
-app.get('/', (req, res, next)=>{
+app.get('/*', (req, res, next)=>{
     res.send('<h1>Hello, world.</h1>');
 });
