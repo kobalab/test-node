c00d010ced7a98f6fed29c8a1b180f3e498948b4
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-09-24 13:56:34 +0800

    404 Not Found の表示をカスタマイズした

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const app = express();
 
 app.use(express.static('./'));
 app.use(index('./', {icons: true, view: 'details'}));
+app.use((req, res)=>res.status(404).send('<h1>Not Found</h1>'))
 
 const server = app.listen(8000, ()=>{
     console.log('Server listening on http://127.0.0.1:8000')
