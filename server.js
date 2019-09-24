194ca388dd0dbee04e2b80e505eb4edd881f9b0d
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-09-24 10:35:22 +0800

    カレントディレクトリをDocumentRootにする簡易HTMLサーバに変更

--- server.js
+++ server.js
@@ -5,11 +5,13 @@
 "use strict";
 
 const express = require('express');
+const index   = require('serve-index');
+
 const app = express();
+
+app.use(express.static('./'));
+app.use(index('./', {icons: true, view: 'details'}));
+
 const server = app.listen(8000, ()=>{
     console.log('Server listening on http://127.0.0.1:8000')
-});
-
-app.get('/*', (req, res, next)=>{
-    res.send('<h1>Hello, world.</h1>');
 });
