c6760fa446ae3554ed4a8f592724ee27484d00d0
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-09-25 15:58:10 +0800

    セッション管理を追加

--- server.js
+++ server.js
@@ -7,10 +7,16 @@
 const express = require('express');
 const index   = require('serve-index');
 const logger  = require('morgan');
+const session = require('express-session');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false
+}));
 app.use(express.static('./'));
 app.use(index('./', {icons: true, view: 'details'}));
 app.use((req, res)=>res.status(404).send('<h1>Not Found</h1>'))
