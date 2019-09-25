31ca88029822b2aa04adf08644628902f706ed87
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-09-25 19:18:17 +0800

    認証用のURLを /login 固定にした

--- server.js
+++ server.js
@@ -16,20 +16,10 @@ app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
 app.use(logger('dev'));
-app.use(session({
-    secret: 'secret',
-    resave: false,
-    saveUninitialized: false
-}));
+app.use(session({secret:'secret', resave:false, saveUninitialized:false}));
 app.use(parser.urlencoded({extended: false}));
-app.get('/*', (req, res, next)=>{
-    if (req.query.login === undefined) return next();
-    res.render('login', {url: req.path});
-})
-app.post('/*', (req, res, next)=>{
-    console.log(req.body);
-    res.redirect(req.path);
-});
+app.get('/login', (req, res, next)=>res.render('login'));
+app.post('/login', (req, res, next)=>res.redirect('/'));
 app.use(express.static('./'));
 app.use(index('./', {icons: true, view: 'details'}));
 app.use((req, res)=>res.status(404).send('<h1>Not Found</h1>'))
