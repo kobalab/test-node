31ef3b0c07290215dd85a7042c7682373c4ac8f2
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-09-25 17:01:45 +0800

    ログインフォームを追加

--- server.js
+++ server.js
@@ -11,12 +11,22 @@ const session = require('express-session');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
 app.use(logger('dev'));
 app.use(session({
     secret: 'secret',
     resave: false,
     saveUninitialized: false
 }));
+app.get('/*', (req, res, next)=>{
+    if (req.query.login === undefined) return next();
+    res.render('login', {url: req.path});
+})
+app.post('/*', (req, res, next)=>{
+    res.redirect(req.path);
+});
 app.use(express.static('./'));
 app.use(index('./', {icons: true, view: 'details'}));
 app.use((req, res)=>res.status(404).send('<h1>Not Found</h1>'))
