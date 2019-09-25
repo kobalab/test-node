8dacecbe17cbd39637ff397de4eef91a6d7edc35
Satoshi Kobayashi <koba@yk.rim.or.jp>
2019-09-25 11:10:24 +0800

    不要な変数 server を削除

--- server.js
+++ server.js
@@ -15,6 +15,4 @@ app.use(express.static('./'));
 app.use(index('./', {icons: true, view: 'details'}));
 app.use((req, res)=>res.status(404).send('<h1>Not Found</h1>'))
 
-const server = app.listen(8000, ()=>{
-    console.log('Server listening on http://127.0.0.1:8000')
-});
+app.listen(8000, ()=>console.log('Server listening on http://127.0.0.1:8000'));
